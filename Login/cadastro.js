f443f79057b3e6605d5322d94b186ad017c4b4c7
Lucas Lopes - Desenvolvedor <ll.lopes2506@gmail.com>
2023-11-28 17:37:02 -0300

    validações, - nome, data, fixo

--- Login/cadastro.js
+++ Login/cadastro.js
@@ -11,32 +11,118 @@ function voltar() {
   window.location = "";
 }
 
-// Permitir apenas letras no usuario e senha
-function validarLetras(event) {
-  // Obtenha o valor do input
-  const valor = event.target.value;
+const maeInput = document.getElementById("mae");
+const labelMae = document.getElementById("labelMae");
 
-  // Verifique se o valor contém um número
-  const temNumero = /[0-9]/.test(valor);
+function validateMae() {
+  const mae = maeInput.value;
 
-  // Se o valor conter um número, remova o número
-  if (temNumero) {
-    event.target.value = valor.replace(/[0-9]/g, "");
+  if (mae.length < 6) {
+    labelMae.textContent = "Nome da mãe é muito curto";
+    labelMae.style.color = "red";
+  } else {
+    labelMae.textContent = "Nome da mãe";
+    labelMae.style.color = "green";
+  }
+}
+maeInput.addEventListener("input", validateMae);
+
+const cpfInput = document.getElementById("cpf");
+const labelCpf = document.getElementById("labelCpf");
+
+function validateCpf() {
+  const cpf = cpfInput.value;
+
+  if (cpf.length < 14) {
+    labelCpf.textContent = "CPF é inválido";
+    labelCpf.style.color = "red";
+  } else {
+    labelCpf.textContent = "CPF válido";
+    labelCpf.style.color = "green";
+  }
+}
+cpfInput.addEventListener("input", validateCpf);
+
+const celularInput = document.getElementById("celular");
+const labelCelular = document.getElementById("labelCelular");
+
+function validateCelular() {
+  const celular = celularInput.value;
+
+  if (celular.length < 15) {
+    labelCelular.textContent = "O número é inválido";
+    labelCelular.style.color = "red";
+  } else {
+    labelCelular.textContent = "Número válido";
+    labelCelular.style.color = "green";
+  }
+}
+celularInput.addEventListener("input", validateCelular);
+
+const usuarioInput = document.getElementById("usuario");
+const labelUsuario = document.getElementById("labelUsuario");
+
+function validateUsername() {
+  const usuario = usuarioInput.value;
+
+  if (usuario.length < 6) {
+    labelUsuario.textContent = "O usuário deve ter  6 caracteres";
+    labelUsuario.style.color = "red";
+  } else {
+    labelUsuario.textContent = "Nome de usuário válido";
+    labelUsuario.style.color = "green";
   }
 }
 
-let senha = document.querySelector("#senha");
-let ConfirmaSenha = document.querySelector("#confirmar_senha");
+usuarioInput.addEventListener("input", validateUsername);
 
-// Adicione o evento ao input
-document.querySelector("#usuario").addEventListener("input", validarLetras);
-document.querySelector("#senha").addEventListener("input", validarLetras);
-document
-  .querySelector("#confirmar_senha")
-  .addEventListener("input", validarLetras);
+usuarioInput.addEventListener("keydown", function (event) {
+  // Verifica se a tecla pressionada é um número
+  const key = event.key;
+  if (key.match(/[0-9]/)) {
+    // Retorna false para impedir que o número seja inserido
+    event.preventDefault();
+  }
+});
 
-function cadastrar() {
-  window.alert("Cadastro concluído!");
+const passwordInput = document.getElementById("senha");
+const confirmPasswordInput = document.getElementById("confirmSenha");
+const labelSenha = document.getElementById("labelSenha");
+const labelConfirmSenha = document.getElementById("labelConfirmSenha");
+
+function validatePasswords() {
+  const password = passwordInput.value;
+  const confirmPassword = confirmPasswordInput.value;
+
+  if (password.length < 8) {
+    labelSenha.textContent = "Obrigatório 8 letras";
+    labelSenha.style.color = "red";
+    labelConfirmSenha.textContent = "Obrigatório 8 letras";
+    labelConfirmSenha.style.color = "red";
+  } else {
+    if (password === confirmPassword) {
+      labelSenha.textContent = "As senhas coincidem";
+      labelSenha.style.color = "green";
+      labelConfirmSenha.textContent = "As senhas coincidem";
+      labelConfirmSenha.style.color = "green";
+    } else {
+      labelSenha.textContent = "As senhas não coincidem";
+      labelSenha.style.color = "red";
+      labelConfirmSenha.textContent = "As senhas não coincidem";
+      labelConfirmSenha.style.color = "red";
+    }
+  }
 }
 
-const form = document.querySelector("#form");
+passwordInput.addEventListener("keyup", validatePasswords);
+confirmPasswordInput.addEventListener("keyup", validatePasswords);
+
+function preventNumberInput(event) {
+  const key = event.key;
+  if (key.match(/[0-9]/)) {
+    event.preventDefault();
+  }
+}
+
+passwordInput.addEventListener("keydown", preventNumberInput);
+confirmPasswordInput.addEventListener("keydown", preventNumberInput);
