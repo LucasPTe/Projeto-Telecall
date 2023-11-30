a782109ac951920ff4c13697bcc131cfe582e7f9
Annibal Gulias <147448973+annibalgulias@users.noreply.github.com>
2023-11-30 13:05:14 -0300

    quase pronto

--- Tela inicial/dark-mode.js
+++ Tela inicial/dark-mode.js
@@ -1,4 +1,24 @@
-function toggleDarkMode() {
-  const body = document.body;
-  body.classList.toggle("dark-mode");
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const body = document.getElementById('body');
+
+ 
+  const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+ 
+  if (isDarkMode) {
+      body.classList.add('dark-mode');
+      darkModeToggle.checked = true;
+  }
+
+ 
+  darkModeToggle.addEventListener('change', function () {
+      if (darkModeToggle.checked) {
+          body.classList.add('dark-mode');
+          localStorage.setItem('darkMode', 'enabled');
+      } else {
+          body.classList.remove('dark-mode');
+          localStorage.setItem('darkMode', null);
+      }
+  });
+});
